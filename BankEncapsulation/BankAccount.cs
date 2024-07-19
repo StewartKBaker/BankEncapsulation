f2f25b04c67a49734a731efb3940be84cc4445b5
StewartBaker <bakrstewart16@gmail.com>
2024-07-19 10:53:11 -0500

    finished exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,72 @@
+namespace BankEncapsulation;
+
+public class BankAccount
+{
+    private double _balance = 14234;
+
+    private double _userPassword = 92615;
+
+    public bool UserPassword(double password)
+    {
+        if (password == _userPassword)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    public void DepositMoney(double m)
+    {
+        _balance += m;
+    }
+
+    public double GetBalance()
+    {
+        return _balance;
+    }
+
+    public void WithdrawMoney(double m)
+    {
+        _balance -= m;
+    }
+
+    public void Interface()
+    {
+        Console.WriteLine("Hi Welcome to your bank account!");
+        Console.WriteLine("\nTo deposit, Press 1. \nTo withdraw, Press 2. \nTo check your balance, Press 3. \nFor customer support, Press 4.\nTo exit, Press 5");
+        bool exit = false;
+        while (!exit)
+        {
+            var userInput = Console.ReadLine();
+            switch (int.Parse(userInput))
+            {
+                case 1:
+                    Console.WriteLine("How much money would you like to deposit?");
+                    var deposit = double.Parse(Console.ReadLine());
+                    DepositMoney(deposit);
+                    Console.WriteLine($"\nYour new balance is ${GetBalance()}");
+                    break;
+                case 2:
+                    Console.WriteLine("How much money would you like to withdraw?");
+                    var withdraw = double.Parse(Console.ReadLine());
+                    WithdrawMoney(withdraw);
+                    Console.WriteLine($"\nYour new balance is ${GetBalance()}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Your Current balance is ${GetBalance()}");
+                    break;
+                case 4:
+                    Console.WriteLine("The current wait time is approximately 4 hours, please hold.");
+                    exit = true;
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Please input a valid number.");
+                    break;
+            }
+             if (!exit) Console.WriteLine("1: Deposit / 2: Withdraw / 3: Check Balance / 4: Customer Support / 5: Exit");
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace BankEncapsulation
+﻿using System.Reflection.Metadata;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myBankAccount = new BankAccount();
+            
+            Console.WriteLine("(Password is 92615)\nHello and welcome to Bank of America!\nUsername: ILoveMoney22\nPassword: ");
+            bool exit = false;
+            while (!exit)
+            {
+                var userPass = int.Parse(Console.ReadLine());
+                if (myBankAccount.UserPassword(userPass))
+                {
+                    myBankAccount.Interface();
+                    exit = true;
+                }
+                else if (!myBankAccount.UserPassword(userPass))
+                {
+                    Console.WriteLine("Invalid Password");
+                }
+            }
         }
     }
 }
